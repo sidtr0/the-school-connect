014290d8d7bf7b8aee777bc3e16419c082911099
Drac <46294577+Drac5079@users.noreply.github.com>
2020-03-08 14:42:45 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import store from "./store/index.js";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyC6l1Htyhiw5-hALd51SJNKIfRCefaFgw4",
-  authDomain: "the-school-connect.firebaseapp.com",
-  databaseURL: "https://the-school-connect.firebaseio.com",
-  projectId: "the-school-connect",
-  storageBucket: "the-school-connect.appspot.com",
-  messagingSenderId: "926697961879",
-  appId: "1:926697961879:web:6ce075a8c1e886810c6cd2"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
